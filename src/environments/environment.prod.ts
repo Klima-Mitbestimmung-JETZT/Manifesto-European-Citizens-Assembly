b091fbba30a38119a859128bc640c8400a711d6c
Enno Rosinger <enno@rosinger.net>
2020-11-23 23:14:09 +0100

    copy env variables to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    space: '5z21gx3h7sb2',
+    environment: "master",
+    accessToken: 'ecQ2l4UMTYi1MU3Szr2iNLep8Ak_pNp5i3Nxe1qU3VA',
+  },
+  mailService:{
+    url: 'https://open-letter-mailer.herokuapp.com'
+  }
 };
